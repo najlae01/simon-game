a2957a2495fd0e4883c675de15788fbdc17b5f47
Najlae Abarghache <88176530+najlae01@users.noreply.github.com>
2022-10-08 00:01:37 +0200

    Update game.js

--- game.js
+++ game.js
@@ -55,7 +55,6 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
